8caac1ea60ef22c8eed95c1ddc23f1c6fff67800
Miguel Angel Mora Ortiz <125917913+Mmora0@users.noreply.github.com>
2023-11-23 13:48:46 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Número 1
+
+//var numero1 = 5;
+
+//var numero2 = 7;
+
+//Console.WriteLine("Por favor, ingresa la suma de " + numero1 + " y " + numero2 + ": ");
+
+//var respuesta = Console.ReadLine();
+
+//var resultado = Convert.ToInt32(respuesta);
+
+//var esCorrecto = resultado == (numero1 + numero2); Console.WriteLine("Tu respuesta es " + esCorrecto);
+
+
+
+//Número 2
+
+//var numero = 7;
+
+//Console.WriteLine("Por favor, ingresa el doble de " + numero + ": ");
+
+//var respuesta = Console.ReadLine();
+
+//var resultado = Convert.ToInt32(respuesta);
+
+//var esCorrecto = resultado == (numero * 2);
+
+//Console.WriteLine("Tu respuesta es " + esCorrecto);
+
+
+
+//Número 3
+
+//var numero = 42;
+
+//Console.WriteLine("Por favor, ingresa el número " + numero + ": ");
+
+//var respuesta = Console.ReadLine();
+
+//var numeroIngresado = Convert.ToInt32(respuesta);
+
+//var esCorrecto = numeroIngresado == numero;
+
+//Console.WriteLine("Tu respuesta es " + esCorrecto);
+
+
+//Número 4
+
+//var numero = 10;
+
+//Console.WriteLine("El valor inicial es " + numero);
+
+//numero++;
+
+//Console.WriteLine("Después de incrementar, el valor es " + numero);
+
+//numero--;
+
+//Console.WriteLine("Después de decrementar, el valor es " + numero);
+
+
+
+//Número 5
+
+//const double PI = 3.14159265359;
+
+//double radio = 5.0; double area = PI * radio * radio;
+
+//Console.WriteLine("El valor de PI es constante: " + PI);
+
+//Console.WriteLine("Dado un radio de " + radio + " unidades,");
+
+//Console.WriteLine("El área de un círculo es: " + area + " unidades cuadradas.");
